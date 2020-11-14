bd298f9087c321ed7cde8a7ec226525859f46459
nafislestamanta <53818473+nafislestamanta@users.noreply.github.com>
2020-11-14 15:50:13 +0700

    login guru seadanya

---
+++ application/views/guru/login.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Login</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--===============================================================================================-->
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="<?= base_url('assets/'); ?> vendor/bootstrap/css/bootstrap.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="<?= base_url('assets/'); ?>fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="<?= base_url('assets/'); ?>fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="<?= base_url('assets/'); ?>vendor/animate/animate.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="<?= base_url('assets/'); ?>vendor/css-hamburgers/hamburgers.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="<?= base_url('assets/'); ?>vendor/animsition/css/animsition.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="<?= base_url('assets/'); ?>vendor/select2/select2.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="<?= base_url('assets/'); ?>vendor/daterangepicker/daterangepicker.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="<?= base_url('assets/'); ?>css/util.css">
+    <link rel="stylesheet" type="text/css" href="<?= base_url('assets/'); ?>css/main.css">
+    <!--===============================================================================================-->
+</head>
+
+<body>
+
+    <div class="limiter">
+        <div class="container-login100">
+            <div class="wrap-login100 p-t-50 p-b-90">
+                <form class="login100-form validate-form flex-sb flex-w" method="post" action="<?= base_url('Guru/auth_guru'); ?>">
+                    <span class="login100-form-title p-b-25">
+                        Login
+                    </span>
+                    <?= $this->session->flashdata('message'); ?>
+                    <div class="wrap-input100 validate-input m-b-7" data-validate="nip is required">
+                        <input class="input100" type="text" id="nip" name="nip" placeholder="Username" value="<?= set_value('nip'); ?>">
+                        <span class="focus-input100"></span>
+                    </div>
+                    <?= form_error('nip', '<small style="color:red" class="text-danger pl-3">', '</small>'); ?>
+
+                    <div class="wrap-input100 validate-input m-t-16 m-b-7" data-validate="Password is required">
+                        <input class="input100" type="password" id="password" name="password" placeholder="Password">
+                        <span class="focus-input100"></span>
+                    </div>
+                    <?= form_error('password', '<small style="color:red" class="text-danger pl-3">', '</small>'); ?>
+
+                    <div class="flex-sb-m w-full p-t-16 p-b-24">
+                        <div class="contact100-form-checkbox">
+                            <input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
+                            <label class="label-checkbox100" for="ckb1">
+                                Remember me
+                            </label>
+                        </div>
+
+                        <div>
+                            <a href="#" class="txt1">
+                                Forgot?
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="container-login100-form-btn m-t-17">
+                        <button type="submit" class="login100-form-btn">
+                            Login
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+
+    <div id="dropDownSelect1"></div>
+
+    <!--===============================================================================================-->
+    <script src="<?= base_url('assets/'); ?>vendor/jquery/jquery-3.2.1.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="<?= base_url('assets/'); ?>vendor/animsition/js/animsition.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="<?= base_url('assets/'); ?>vendor/bootstrap/js/popper.js"></script>
+    <script src="<?= base_url('assets/'); ?>vendor/bootstrap/js/bootstrap.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="<?= base_url('assets/'); ?>vendor/select2/select2.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="<?= base_url('assets/'); ?>vendor/daterangepicker/moment.min.js"></script>
+    <script src="<?= base_url('assets/'); ?>vendor/daterangepicker/daterangepicker.js"></script>
+    <!--===============================================================================================-->
+    <script src="<?= base_url('assets/'); ?>vendor/countdowntime/countdowntime.js"></script>
+    <!--===============================================================================================-->
+    <script src="<?= base_url('assets/'); ?>js/main.js"></script>
+
+</body>
+
+</html>

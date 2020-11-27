9fa8d351f8f7c62e319b4df06bbbb274a5ba06fd
Nizarudn <muhnizarudn@gmail.com>
2020-11-27 21:26:38 +0700

    dashboard-guru

---
+++ application/models/m_absen_siswa.php
@@ -0,0 +1,76 @@
+<?php
+
+class m_absen_siswa extends CI_Model
+{
+    public function TampilAbsen()
+    {
+        return $this->db->order_by('tgl', 'DESC')->get('
+        ')->result();
+    }
+
+    public function insert($data)
+    {
+        return $this->db->insert('absen_guru', $data);
+    }
+
+    public function delete($id)
+    {
+        return $this->db->where('id_absen', $id)->delete('absen_guru');
+    }
+
+    public function update($data, $id)
+    {
+        return $this->db->where('id_absen', $id)->update('absen_guru', $data);
+    }
+    public function TampilWalikelas()
+    {
+        $this->db->select('*');
+        $this->db->from('wali_kelas');
+        $this->db->join('guru', 'guru.nip=wali_kelas.nip');
+        $this->db->join('penjurusan', 'penjurusan.id_jurusan=wali_kelas.id_jurusan');
+        $this->db->join('kelas', 'kelas.id_kelas=penjurusan.id_kelas');
+        $query = $this->db->get();
+        return $query;
+    }
+
+    public function insert($data)
+    {
+        return $this->db->insert('wali_kelas', $data);
+    }
+
+    public function getAllGuru()
+    {
+        return $this->db->get('guru')->result();
+    }
+
+    public function joinkelasjurusan()
+    {
+        $this->db->select('*');
+        $this->db->from('penjurusan');
+        $this->db->join('kelas', 'kelas.id_kelas=penjurusan.id_kelas');
+        $query = $this->db->get();
+        return $query;
+    }
+
+    public function delete($id)
+    {
+        return $this->db->where('id_walikelas', $id)->delete('wali_kelas');
+    }
+
+    public function get($id)
+    {
+        $this->db->select('*');
+        $this->db->from('wali_kelas');
+        $this->db->join('guru', 'guru.nip=wali_kelas.nip');
+        $this->db->join('penjurusan', 'penjurusan.id_jurusan=wali_kelas.id_jurusan');
+        $this->db->join('kelas', 'kelas.id_kelas=penjurusan.id_kelas');
+        $this->db->where('wali_kelas.id_walikelas', $id);
+        $query = $this->db->get();
+        return $query;
+    }
+
+    public function update($data, $id)
+    {
+        return $this->db->where('id_walikelas', $id)->update('wali_kelas', $data);
+    }
+}

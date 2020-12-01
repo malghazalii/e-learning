ac903a26385fbd856ea5d76a1d7aa4c4332a4c0b
nafislestamanta <53818473+nafislestamanta@users.noreply.github.com>
2020-12-02 03:38:25 +0700

    katutt

--- application/views/admin/templates/sidebar.php
+++ application/views/admin/templates/sidebar.php
@@ -28,11 +28,11 @@
        <?php else : ?>
        <li class="nav-item">
        <?php endif; ?>
-       <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+       <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
          <i class="fas fa-fw fa-layer-group"></i>
          <span>Data Guru</span>
        </a>
-       <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+       <div id="collapseOne" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
          <div class="bg-white py-2 collapse-inner rounded">
            <h6 class="collapse-header">Data Guru:</h6>
            <?php if ($title == 'Guru') : ?>

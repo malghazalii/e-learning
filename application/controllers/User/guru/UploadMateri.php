cdeb3785eafd1b95e50e0d269805e3e9e3e22b41
nafislestamanta <53818473+nafislestamanta@users.noreply.github.com>
2020-12-02 02:06:37 +0700

    upload materi

--- application/controllers/User/guru/UploadMateri.php
+++ application/controllers/User/guru/UploadMateri.php
@@ -3,13 +3,72 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class UploadMateri extends CI_Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('form_validation');
+    }
+
     public function index()
     {
+        $nip = $this->session->userdata('nip');
+        $queryMengajar = "SELECT * FROM `mengajar` JOIN guru on guru.nip = mengajar.nip JOIN mata_pelajaran 
+        on mata_pelajaran.id_mapel = mengajar.id_mapel join penjurusan on penjurusan.id_jurusan = mengajar.id_jurusan JOIN kelas 
+        on kelas.id_kelas = penjurusan.id_kelas  WHERE mengajar.nip = $nip";
+
+        $data['mengajar'] = $this->db->query($queryMengajar)->result();
         $data['title'] = 'Upload Materi';
         $this->load->view('users/templates/header', $data);
         $this->load->view('users/templates/navguru');
         $this->load->view('users/templates/navPilihan', $data);
         $this->load->view('users/guru/uploadmateri');
         $this->load->view('users/templates/footer');
+    }
+
+    public function tambahData()
+    {
+        $this->form_validation->set_rules('nama', 'Nama', 'required|trim', [
+            'required' => 'Field tidak boleh kosong'
+        ]);
+
+        if ($this->form_validation->run() == false) {
+            $this->index();
+        } else {
+            $mapel = $this->input->post('mapel');
+            $nama = $this->input->post('nama');
+            $keterangan = $this->input->post('keterangan');
+            $file = $_FILES['file'];
+            if ($file = '') {
+            } else {
+                $config['upload_path']  =   './assets/users/upload';
+                $config['allowed_types'] =  'pdf|docx|pptx';
+
+                $this->load->library('upload', $config);
+                if (!$this->upload->do_upload('file')) {
+                    echo "Upload Gagal";
+                    die();
+                } else {
+                    $file = $this->upload->data('file_name');
+                }
+            }
+
+            $data = [
+                'judul' => $nama,
+                'id_mengajar' => $mapel,
+                'nama_file' => $file,
+                'tgl_posting' => date('y-m-d'),
+                'isi' => $keterangan
+            ];
+
+            $simpan = $this->db->insert('materi', $data);
+
+            if ($simpan) {
+                $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Materi berhasil diupload</div>');
+            } else {
+                $this->session->set_flashdata('messgae', '<div class="alert alert-danger" role="alert">Materi gagal diupload</div>');
+            }
+
+            redirect('User/Guru/Dashboard');
+        }
     }
 }
--- application/views/users/guru/uploadmateri.php
+++ application/views/users/guru/uploadmateri.php
@@ -1,80 +1,48 @@
         <div class="card-body">
-            <form class="form-horizontal" role="form">
-                <div class="form-group">
-                    <div class="row">
-                        <label for="birthDate" class="col-sm-3 control-label">Pilih Mata Pelajaran</label>
-                        <div class="col-sm-9">
-                            <select id="country" class="form-control">
-                                <option>Fisika</option>
-                                <option>Biologi</option>
-                                <option>Kimia</option>
-                            </select>
-                        </div>
+            <?php echo form_open_multipart('user/guru/uploadmateri/tambahData'); ?>
+            <div class="form-group">
+                <div class="row">
+                    <label for="birthDate" class="col-sm-3 control-label">Pilih Mata Pelajaran</label>
+                    <div class="col-sm-9">
+                        <select id="country" class="form-control" name="mapel" id="mapel">
+                            <?php foreach ($mengajar as $m) : ?>
+                                <option value="<?= $m->id_mengajar ?>"> <?= $m->mata_pelajaran ?> - <?= $m->kelas ?> <?= $m->nama_jurusan ?> </option>
+                            <?php endforeach; ?>
+                        </select>
                     </div>
                 </div>
-
-                <div class="form-group">
-                    <div class="row">
-                        <label for="birthDate" class="col-sm-3 control-label">Kelas - Jurusan</label>
-                        <div class="col-sm-3">
-                            <select id="country" class="form-control">
-                                <option>10</option>
-                                <option>11</option>
-                                <option>12</option>
-                            </select>
-                        </div>
-                        <div class="col-sm-6">
-                            <select id="country" class="form-control">
-                                <option>MIPA-1</option>
-                                <option>MIPA-2</option>
-                                <option>IPS-1</option>
-                                <option>IPS-2</option>
-                            </select>
-                        </div>
-
+            </div>
+            <div class="form-group">
+                <div class="row">
+                    <label for="namatugas" class="col-sm-3 control-label">Nama Materi</label>
+                    <div class="col-sm-9">
+                        <input type="text" id="nama" name="nama" placeholder="nama materi" class="form-control" autofocus>
                     </div>
                 </div>
-                <div class="form-group">
-                    <div class="row">
-                        <label for="namatugas" class="col-sm-3 control-label">Nama Materi</label>
-                        <div class="col-sm-9">
-                            <input type="text" id="namatugas" placeholder="nama tugas" class="form-control" autofocus>
-                        </div>
+            </div>
+            <div class="form-group">
+                <div class="row">
+                    <label for="keterangan" class="col-sm-3 control-label">Keterangan</label>
+                    <div class="col-sm-9">
+                        <textarea rows="4" cols="97" name="keterangan" id="keterangan" class="form-control" placeholder="keterangan"></textarea>
                     </div>
                 </div>
-                <div class="form-group">
-                    <div class="row">
-                        <label for="keterangan" class="col-sm-3 control-label">Keterangan</label>
-                        <div class="col-sm-9">
-                            <input type="text" id="Keterangan" placeholder="keterangan" class="form-control">
-                        </div>
+            </div>
+            <div class="form-group">
+                <div class="row">
+
+                    <label for="keterangan" class="col-sm-3 control-label">Masukkan File</label>
+                    <div class="col-sm-9">
+                        <input type="file" name="file" id="file" class="btn btn-info upload">
                     </div>
                 </div>
-                <div class="form-group">
-                    <div class="row">
-
-                        <label for="keterangan" class="col-sm-3 control-label">Masukkan File</label>
-                        <div class="col-sm-9">
-                            <input type="file" name="gambar_soal" id="gambar_soal" class="btn btn-info upload">
-                        </div>
-                    </div>
-                </div>
-                <div class="form-group">
-
-
-
-                </div> <!-- /.form-group -->
-                <div class="form-group">
-                </div> <!-- /.form-group -->
-        </div> <!-- /.form-group -->
-        <div class="form-group">
-        </div> <!-- /.form-group -->
-        <div class="form-group">
+            </div>
             <div class="col-sm-3 col-sm-offset-3">
                 <button type="submit" class="btn btn-primary btn-block">Simpan</button>
             </div>
+            <?php echo form_close(); ?>
         </div>
-        </form> <!-- /form -->
+        <!-- /form -->
 
         </div>
         </div>

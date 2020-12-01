1a3c6524171445b20dc0f0b52b475aef3aa34a63
nafislestamanta <53818473+nafislestamanta@users.noreply.github.com>
2020-12-01 16:08:03 +0700

    - combo box

--- application/controllers/User/guru/EssayEvent.php
+++ application/controllers/User/guru/EssayEvent.php
@@ -5,11 +5,16 @@ class EssayEvent extends CI_Controller
 {
     public function index()
     {
+        $nip = $this->session->userdata('nip');
+        $queryMengajar = "SELECT * FROM `mengajar` JOIN guru on guru.nip = mengajar.nip JOIN mata_pelajaran 
+        on mata_pelajaran.id_mapel = mengajar.id_mapel join penjurusan on penjurusan.id_jurusan = mengajar.id_jurusan JOIN kelas 
+        on kelas.id_kelas = penjurusan.id_kelas  WHERE mengajar.nip = $nip";
+
         $data['title'] = 'Essay Event';
+        $data['mengajar'] = $this->db->query($queryMengajar)->result();
         $this->load->view('users/templates/header', $data);
         $this->load->view('users/templates/navguru');
         $this->load->view('users/guru/create_event/esai');
         $this->load->view('users/templates/footer');
-
     }
 }

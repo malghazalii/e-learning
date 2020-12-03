02739001dea3574ba15cb7c6c68c40595dc9c362
Nizarudn <muhnizarudn@gmail.com>
2020-12-04 02:25:20 +0700

    dashboard 75%

--- application/views/users/guru/dashboard.php
+++ application/views/users/guru/dashboard.php
@@ -1,120 +1,37 @@
-<!-- course details -->
-<div class="details-w3l py-5">
-		<div class="container py-xl-5 py-lg-3">
-
-			<div class="row inner_sec_info py-xl-5 py-lg-3">
-				<!-- left side -->
-				<div class="col-lg-8 single-left">
-					<div class="single-left1">
-						<h6 class="details-heading font-italic my-4">E-Learning SMAN 1 Bondowoso</h6>
-						<h5 class="card-title">
-							<a href="course_details.html" class="text-dark">Sed ut perspiciatis unde omnis iste natus error sit facilisis erat posuere erat</a>
-						</h5>
-						<p>In consectetur, elit in scelerisque malesuada, tellus leo viverra massa, nec malesuada turpis lectus ac urna. Suspendisse
-							ut neque orci. Phasellus tellus diam, pulvinar sit amet elit sit amet, fermentum viverra eros. Praesent neque purus,
-							rhoncus nec nibh non, mollis sodales odio. Nullam facilisis diam non magna porta luctus. Aenean facilisis erat posuere
-							erat ornare ultrices. Aliquam ac arcu interdum, dapibus nibh convallis, semper augue. In scelerisque malesuada, tellus
-							leo viverra massa, nec malesuada turpis lectus ac urna. Suspendisse ut neque orci. Phasellus tellus diam, pulvinar
-							sit amet elit sit amet, fermentum viverra eros. Praesent neque purus, rhoncus nec nibh non, mollis sodales odio.</p>
-						<p class="mt-3">Aenean facilisis erat posuere erat ornare ultrices. Aliquam ac arcu interdum, dapibus nibh convallis, semper augue.
-							In scelerisque malesuada, tellus leo viverra massa, nec malesuada turpis lectus ac urna. Suspendisse ut neque orci.
-							Phasellus tellus diam, pulvinar sit amet elit sit amet, fermentum viverra eros. Praesent neque purus, rhoncus nec
-							nibh non, mollis sodales odio.</p>
-					</div>
-				</div>
-				<!-- //left side -->
-				<!-- right side -->
-				<div class="col-lg-4 event-right mt-lg-0 mt-sm-5 mt-4">
-					<div class="event-right1">
-						<div class="categories my-4 p-4 border">
-							<h3 class="blog-title text-dark">Data Mata Pelajaran</h3>
-							<ul>
-								<li class="mt-3">
-									<i class="fas fa-check mr-2"></i>
-									<a href="language.html">Language</a>
-								</li>
-								<li class="mt-3">
-									<i class="fas fa-check mr-2"></i>
-									<a href="communication.html">Communication</a>
-								</li>
-								<li class="mt-3">
-									<i class="fas fa-check mr-2"></i>
-									<a href="business.html">Business</a>
-								</li>
-								<li class="mt-3">
-									<i class="fas fa-check mr-2"></i>
-									<a href="software.html">Software</a>
-								</li>
-								<li class="mt-3">
-									<i class="fas fa-check mr-2"></i>
-									<a href="social_media.html">Social Media</a>
-								</li>
-								<li class="mt-3">
-									<i class="fas fa-check mr-2"></i>
-									<a href="photography.html">Photography</a>
-								</li>
-							</ul>
-						</div>
-						<div class="posts p-4 border">
-							<h3 class="blog-title text-dark">Tugas yang baru anda tambahkan</h3>
-							<div class="posts-grids">
-								<div class="row posts-grid mt-4">
-									<div class="col-lg-4 col-md-3 col-4 posts-grid-left pr-0">
-										<a href="course_details.html">
-										<img src="<?php echo base_url(); ?>assets/users/images/document.png" alt=" " class="img-fluid" />
-										</a>
-									</div>
-									<div class="col-lg-8 col-md-7 col-8 posts-grid-right mt-lg-0 mt-md-5 mt-sm-4">
-										<h4>
-											<a href="course_details.html" class="text-dark">Sed ut perspiciatis unde omni</a>
-										</h4>
-										<ul class="wthree_blog_events_list mt-2">
-											<li class="mr-2 text-dark">
-												<i class="far fa-calendar-alt  mr-2"></i>4 Months</li>
-											
-										</ul>
-									</div>
-								</div>
-								<div class="row posts-grid mt-4">
-									<div class="col-lg-4 col-md-3 col-4 posts-grid-left pr-0">
-										<a href="course_details.html">
-										<img src="<?php echo base_url(); ?>assets/users/images/document.png" alt=" " class="img-fluid" />
-										</a>
-									</div>
-									<div class="col-lg-8 col-md-7 col-8 posts-grid-right mt-lg-0 mt-md-5 mt-sm-4">
-										<h4>
-											<a href="course_details.html" class="text-dark">Sed ut perspiciatis unde omni</a>
-										</h4>
-										<ul class="wthree_blog_events_list mt-2">
-											<li class="mr-2 text-dark">
-												<i class="far fa-calendar-alt mr-2"></i>8 Months</li>
-											
-										</ul>
-									</div>
-								</div>
-								<div class="row posts-grid mt-4">
-									<div class="col-lg-4 col-md-3 col-4 posts-grid-left pr-0">
-										<a href="course_details.html">
-										<img src="<?php echo base_url(); ?>assets/users/images/document.png" alt=" " class="img-fluid" />
-										</a>
-									</div>
-									<div class="col-lg-8 col-md-7 col-8 posts-grid-right mt-lg-0 mt-md-5 mt-sm-4">
-										<h4>
-											<a href="course_details.html" class="text-dark">Sed ut perspiciatis unde omni</a>
-										</h4>
-										<ul class="wthree_blog_events_list mt-2">
-											<li class="mr-2 text-dark">
-												<i class="far fa-calendar-alt mr-2"></i>1 Year</li>
-											
-										</ul>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<!-- //right side -->
-			</div>
-		</div>
-	</div>
-	<!-- //course details -->
+<br>
+<div class="container">
+<img src="<?php echo base_url(); ?>assets/users/images/menu.png" style=”float:left; margin:0 8px 4px 0;” /><strong> Ringkasan Tugas</strong>
+<hr>
+<button type="button" class="btn btn-primary">Semua</button>
+<button type="button" class="btn btn-danger">Mata Pelajaran</button>
+<br>
+<br>
+<div class="card">
+  <h5 class="card-header">Tugas Baru</h5>
+  <div class="card-body">
+  <img src="<?php echo base_url(); ?>assets/users/images/doc1.png" style=”float:left; margin:0 8px 4px 0;” /> Nama tugas
+    <p class="card-text-left">12 Desember, 00:00</p>
+	<hr>
+	<img src="<?php echo base_url(); ?>assets/users/images/doc1.png" style=”float:left; margin:0 8px 4px 0;” /> Nama tugas
+    <p class="card-text-left">13 Desember, 00:00</p>
+	<hr>
+	<img src="<?php echo base_url(); ?>assets/users/images/doc1.png" style=”float:left; margin:0 8px 4px 0;” /> Nama tugas
+    <p class="card-text-left">13 Desember, 00:00</p>
+  </div>
+</div>
+<br>
+<div class="card">
+  <h5 class="card-header">Kuis Baru</h5>
+  <div class="card-body">
+  <img src="<?php echo base_url(); ?>assets/users/images/pie.png" style=”float:left; margin:0 8px 4px 0;” /> Nama kuis
+    <p class="card-text-left">12 Desember, 00:00</p>
+	<hr>
+	<img src="<?php echo base_url(); ?>assets/users/images/pie.png" style=”float:left; margin:0 8px 4px 0;” /> Nama kuis
+    <p class="card-text-left">13 Desember, 00:00</p>
+	<hr>
+	<img src="<?php echo base_url(); ?>assets/users/images/pie.png" style=”float:left; margin:0 8px 4px 0;” /> Nama kuis
+    <p class="card-text-left">13 Desember, 00:00</p>
+  </div>
+</div>
+</div>
+<br>
--- application/views/users/siswa/dashboard.php
+++ application/views/users/siswa/dashboard.php
@@ -1,162 +1,37 @@
-<div class="details-w3l py-5">
-		<div class="container">
-
-			<div class="row inner_sec_info py-xl-5 py-lg-3">
-				<!-- left side -->
-				<div class="col-lg-8 single-left">
-					<div class="single-left1">
-						<h6 class="details-heading font-italic my-4">E-Learning SMAN 1 Bondowoso</h6>
-						<h5 class="card-title">
-							<a href="course_details.html" class="text-dark">Sed ut perspiciatis unde omnis iste natus error sit facilisis erat posuere erat</a>
-						</h5>
-						<p>In consectetur, elit in scelerisque malesuada, tellus leo viverra massa, nec malesuada turpis lectus ac urna. Suspendisse
-							ut neque orci. Phasellus tellus diam, pulvinar sit amet elit sit amet, fermentum viverra eros. Praesent neque purus,
-							rhoncus nec nibh non, mollis sodales odio. Nullam facilisis diam non magna porta luctus. Aenean facilisis erat posuere
-							erat ornare ultrices. Aliquam ac arcu interdum, dapibus nibh convallis, semper augue. In scelerisque malesuada, tellus
-							leo viverra massa, nec malesuada turpis lectus ac urna. Suspendisse ut neque orci. Phasellus tellus diam, pulvinar
-							sit amet elit sit amet, fermentum viverra eros. Praesent neque purus, rhoncus nec nibh non, mollis sodales odio.</p>
-						<p class="mt-3">Aenean facilisis erat posuere erat ornare ultrices. Aliquam ac arcu interdum, dapibus nibh convallis, semper augue.
-							In scelerisque malesuada, tellus leo viverra massa, nec malesuada turpis lectus ac urna. Suspendisse ut neque orci.
-							Phasellus tellus diam, pulvinar sit amet elit sit amet, fermentum viverra eros. Praesent neque purus, rhoncus nec
-							nibh non, mollis sodales odio.</p>
-					</div>
-				</div>
-				<!-- //left side -->
-				<!-- right side -->
-				<div class="col-lg-4 event-right mt-lg-0 mt-sm-5 mt-4">
-					<div class="event-right1">
-						<div class="posts p-4 border">
-							<h3 class="blog-title text-dark">Tugas Lampau</h3>
-							<div class="posts-grids">
-								<div class="row posts-grid mt-4">
-									<div class="col-lg-4 col-md-3 col-4 posts-grid-left pr-0">
-										<a href="course_details.html">
-										<img src="<?php echo base_url(); ?>assets/users/images/late-fee.png" alt=" " class="img-fluid" />
-										</a>
-									</div>
-									<div class="col-lg-8 col-md-7 col-8 posts-grid-right mt-lg-0 mt-md-5 mt-sm-4">
-										<h4>
-											<a href="course_details.html" class="text-dark">Sed ut perspiciatis unde omni</a>
-										</h4>
-										<ul class="wthree_blog_events_list mt-2">
-											<li class="mr-2 text-dark">
-												<i class="far fa-calendar-alt  mr-2"></i>4 Months</li>
-												<li class="my-3">
-												<i class="fas fa-clock mr-3"></i>4 - 6 hours</li>
-											
-										</ul>
-									</div>
-								</div>
-								<div class="row posts-grid mt-4">
-									<div class="col-lg-4 col-md-3 col-4 posts-grid-left pr-0">
-										<a href="course_details.html">
-										<img src="<?php echo base_url(); ?>assets/users/images/late-fee.png" alt=" " class="img-fluid" />
-										</a>
-									</div>
-									<div class="col-lg-8 col-md-7 col-8 posts-grid-right mt-lg-0 mt-md-5 mt-sm-4">
-										<h4>
-											<a href="course_details.html" class="text-dark">Sed ut perspiciatis unde omni</a>
-										</h4>
-										<ul class="wthree_blog_events_list mt-2">
-											<li class="mr-2 text-dark">
-												<i class="far fa-calendar-alt mr-2"></i>8 Months</li>
-												<li class="my-3">
-												<i class="fas fa-clock mr-3"></i>4 - 6 hours</li>
-											
-										</ul>
-									</div>
-								</div>
-								<div class="row posts-grid mt-4">
-									<div class="col-lg-4 col-md-3 col-4 posts-grid-left pr-0">
-										<a href="course_details.html">
-										<img src="<?php echo base_url(); ?>assets/users/images/late-fee.png" alt=" " class="img-fluid" />
-										</a>
-									</div>
-									<div class="col-lg-8 col-md-7 col-8 posts-grid-right mt-lg-0 mt-md-5 mt-sm-4">
-										<h4>
-											<a href="course_details.html" class="text-dark">Sed ut perspiciatis unde omni</a>
-										</h4>
-										<ul class="wthree_blog_events_list mt-2">
-											<li class="mr-2 text-dark">
-												<i class="far fa-calendar-alt mr-2"></i>1 Year</li>
-												<li class="my-3">
-												<i class="fas fa-clock mr-3"></i>4 - 6 hours</li>
-											
-										</ul>
-									</div>
-								</div>
-							</div>
-						</div>
-						<br>
-						<div class="posts p-4 border">
-							<h3 class="blog-title text-dark">Tugas Terbaru</h3>
-							<div class="posts-grids">
-								<div class="row posts-grid mt-4">
-									<div class="col-lg-4 col-md-3 col-4 posts-grid-left pr-0">
-										<a href="course_details.html">
-										<img src="<?php echo base_url(); ?>assets/users/images/document.png" alt=" " class="img-fluid" />
-										</a>
-									</div>
-									<div class="col-lg-8 col-md-7 col-8 posts-grid-right mt-lg-0 mt-md-5 mt-sm-4">
-										<h4>
-											<a href="course_details.html" class="text-dark">Sed ut perspiciatis unde omni</a>
-										</h4>
-										<ul class="wthree_blog_events_list mt-2">
-											<li class="mr-2 text-dark">
-												<i class="far fa-calendar-alt  mr-2"></i>4 Months</li>
-												<li class="my-3">
-												<i class="fas fa-clock mr-3"></i>4 - 6 hours</li>
-											
-										</ul>
-									</div>
-								</div>
-								<div class="row posts-grid mt-4">
-									<div class="col-lg-4 col-md-3 col-4 posts-grid-left pr-0">
-										<a href="course_details.html">
-										<img src="<?php echo base_url(); ?>assets/users/images/document.png" alt=" " class="img-fluid" />
-										</a>
-									</div>
-									<div class="col-lg-8 col-md-7 col-8 posts-grid-right mt-lg-0 mt-md-5 mt-sm-4">
-										<h4>
-											<a href="course_details.html" class="text-dark">Sed ut perspiciatis unde omni</a>
-										</h4>
-										<ul class="wthree_blog_events_list mt-2">
-											<li class="mr-2 text-dark">
-												<i class="far fa-calendar-alt mr-2"></i>8 Months</li>
-												<li class="my-3">
-												<i class="fas fa-clock mr-3"></i>4 - 6 hours</li>
-											
-										</ul>
-									</div>
-								</div>
-								<div class="row posts-grid mt-4">
-									<div class="col-lg-4 col-md-3 col-4 posts-grid-left pr-0">
-										<a href="course_details.html">
-										<img src="<?php echo base_url(); ?>assets/users/images/document.png" alt=" " class="img-fluid" />
-										</a>
-									</div>
-									<div class="col-lg-8 col-md-7 col-8 posts-grid-right mt-lg-0 mt-md-5 mt-sm-4">
-										<h4>
-											<a href="course_details.html" class="text-dark">Sed ut perspiciatis unde omni</a>
-										</h4>
-										<ul class="wthree_blog_events_list mt-2">
-											<li class="mr-2 text-dark">
-												<i class="far fa-calendar-alt mr-2"></i>1 Year</li>
-												<li class="my-3">
-												<i class="fas fa-clock mr-3"></i>4 - 6 hours</li>
-											
-										</ul>
-									</div>
-								</div>
-							</div>
-						</div>
-
-
-						
-					</div>
-				</div>
-				<!-- //right side -->
-			</div>
-		</div>
-	</div>
-	<!-- //course details -->
+<br>
+<div class="container">
+<img src="<?php echo base_url(); ?>assets/users/images/menu.png" style=”float:left; margin:0 8px 4px 0;” /><strong> Ringkasan Tugas</strong>
+<hr>
+<button type="button" class="btn btn-primary">Semua</button>
+<button type="button" class="btn btn-danger">Mata Pelajaran</button>
+<br>
+<br>
+<div class="card">
+  <h5 class="card-header">Tugas Terlambat</h5>
+  <div class="card-body">
+  <img src="<?php echo base_url(); ?>assets/users/images/doc1.png" style=”float:left; margin:0 8px 4px 0;” /> Nama tugas
+    <p class="card-text-left">12 Desember, 00:00</p>
+	<hr>
+	<img src="<?php echo base_url(); ?>assets/users/images/pie.png" style=”float:left; margin:0 8px 4px 0;” /> Nama Kuis
+    <p class="card-text-left">13 Desember, 00:00</p>
+	<hr>
+	<img src="<?php echo base_url(); ?>assets/users/images/create.png" style=”float:left; margin:0 8px 4px 0;” /> Absensi
+    <p class="card-text-left">13 Desember, 00:00</p>
+  </div>
+</div>
+<br>
+<div class="card">
+  <h5 class="card-header">Tugas Akan Datang</h5>
+  <div class="card-body">
+  <img src="<?php echo base_url(); ?>assets/users/images/doc1.png" style=”float:left; margin:0 8px 4px 0;” /> Nama tugas
+    <p class="card-text-left">12 Desember, 00:00</p>
+	<hr>
+	<img src="<?php echo base_url(); ?>assets/users/images/pie.png" style=”float:left; margin:0 8px 4px 0;” /> Nama Kuis
+    <p class="card-text-left">13 Desember, 00:00</p>
+	<hr>
+	<img src="<?php echo base_url(); ?>assets/users/images/create.png" style=”float:left; margin:0 8px 4px 0;” /> Absensi
+    <p class="card-text-left">13 Desember, 00:00</p>
+  </div>
+</div>
+</div>
+<br>

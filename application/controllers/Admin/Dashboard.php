e934673769ae824565b5d51bf60dca8ddb3b4b67
Ihza Oktavira <57393641+ihza149@users.noreply.github.com>
2020-12-10 20:26:25 +0700

    Dashboard Admin

---
+++ application/controllers/Admin/Dashboard.php
@@ -0,0 +1,28 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Dashboard extends CI_Controller
+{
+  function __construct()
+  {
+    parent::__construct();
+    $this->load->model('m_datasiswa');
+    //cek_login_admin();
+  }
+
+  public function index()
+  {
+    $jumlahsiswa = "SELECT COUNT(nis) AS jumlah FROM siswa";
+    $jumlahguru = "SELECT COUNT(nip) AS jml FROM guru";
+    $data['jumlahguru'] = $this->db->query($jumlahguru)->row();
+    $data['jumlahsiswa'] = $this->db->query($jumlahsiswa)->row();
+    $data['title'] = 'Dashboard';
+    $data['data'] = $this->db->get_where('admin', ['nip' => $this->session->userdata('nip')])->row_array();
+    $this->load->view('admin/templates/header', $data);
+    $this->load->view('admin/templates/sidebar', $data);
+    $this->load->view('admin/templates/topbar', $data);
+    $this->load->view('admin/dashboard', $data);
+    $this->load->view('admin/templates/footer', $data);
+  }
+}
+?>
---
+++ application/views/admin/dashboard.php
@@ -0,0 +1,46 @@
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+<!-- Page Heading -->
+<div class="d-sm-flex align-items-center justify-content-between mb-4">
+  <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+</div>
+
+<!-- Content Row -->
+<div class="row">
+
+  <!-- Earnings (Monthly) Card Example -->
+  <div class="col-xl-3 col-md-6 mb-4">
+    <div class="card border-left-primary shadow h-100 py-2">
+      <div class="card-body">
+        <div class="row no-gutters align-items-center">
+          <div class="col mr-2">
+            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Data Jumlah Seluruh Siswa</div>
+            <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $jumlahsiswa->jumlah?></div>
+          </div>
+          <div class="col-auto">
+            <i class="fas fa-calendar fa-2x text-gray-300"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Earnings (Monthly) Card Example -->
+  <div class="col-xl-3 col-md-6 mb-4">
+    <div class="card border-left-success shadow h-100 py-2">
+      <div class="card-body">
+        <div class="row no-gutters align-items-center">
+          <div class="col mr-2">
+            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Data jumlah Seluruh Guru</div>
+            <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $jumlahguru->jml?></div>
+          </div>
+          <div class="col-auto">
+            <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>

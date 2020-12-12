e7389dff25eef84e91a2ec9c1085f1e71fa09257
gue-ali <55520049+gue-ali@users.noreply.github.com>
2020-12-13 02:22:51 +0700

    tampil absen
    tampil

--- application/controllers/User/guru/Report_absensi.php
+++ application/controllers/User/guru/Report_absensi.php
@@ -1,14 +1,73 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Report_absensi extends CI_Controller {
+class Report_absensi extends CI_Controller
+{
 
+	function __construct()
+	{
+		parent::__construct();
+
+		//cek_login_admin();
+	}
 	public function index()
-	{$data['title'] = 'Laporan Absen';
+	{
+		$nip = $this->session->userdata('nip');
+		$queryMengajar = "SELECT * FROM `mengajar`
+		JOIN mata_pelajaran ON mata_pelajaran.id_mapel = mengajar.id_mapel 
+		JOIN penjurusan ON penjurusan.id_jurusan = mengajar.id_jurusan 
+		JOIN kelas ON kelas.id_kelas = penjurusan.id_kelas
+		JOIN guru ON guru.nip = mengajar.nip
+		WHERE guru.nip = $nip";
+
+		$ngambilabsen = "SELECT *, siswa.nama as NAMA FROM `tr_absen_siswa` 
+		JOIN siswa ON siswa.nis = tr_absen_siswa.nis
+		JOIN absen_siswa on tr_absen_siswa.id_absen = absen_siswa.id_absen
+		JOIN mengajar ON mengajar.id_mengajar = absen_siswa.id_mengajar
+		JOIN guru ON guru.nip = mengajar.nip
+		JOIN mata_pelajaran ON mata_pelajaran.id_mapel = mengajar.id_mapel
+		JOIN penjurusan ON penjurusan.id_jurusan = mengajar.id_jurusan
+		JOIN kelas ON kelas.id_kelas = penjurusan.id_kelas
+		WHERE guru.nip = $nip 
+		ORDER BY absen_siswa.tanggal ASC";
+
+		$data['title'] = 'Laporan Absen';
+		$data['mengajar'] = $this->db->query($queryMengajar)->result();
+		$data['absensi'] = $this->db->query($ngambilabsen)->result();
+
 		$this->load->view('users/templates/header', $data);
 		$this->load->view('users/templates/navguru');
 		$this->load->view('users/guru/report_absensi');
 		$this->load->view('users/templates/footer');
 	}
+	public function mengajar($id)
+	{
+		$nip = $this->session->userdata('nip');
+		$queryMengajar = "SELECT * FROM `mengajar`
+		JOIN mata_pelajaran ON mata_pelajaran.id_mapel = mengajar.id_mapel 
+		JOIN penjurusan ON penjurusan.id_jurusan = mengajar.id_jurusan 
+		JOIN kelas ON kelas.id_kelas = penjurusan.id_kelas
+		JOIN guru ON guru.nip = mengajar.nip
+		WHERE guru.nip = $nip";
 
+		$ngambilabsen = "SELECT *, siswa.nama as NAMA FROM `tr_absen_siswa` 
+		JOIN siswa ON siswa.nis = tr_absen_siswa.nis
+		JOIN absen_siswa on tr_absen_siswa.id_absen = absen_siswa.id_absen
+		JOIN mengajar ON mengajar.id_mengajar = absen_siswa.id_mengajar
+		JOIN guru ON guru.nip = mengajar.nip
+		JOIN mata_pelajaran ON mata_pelajaran.id_mapel = mengajar.id_mapel
+		JOIN penjurusan ON penjurusan.id_jurusan = mengajar.id_jurusan
+		JOIN kelas ON kelas.id_kelas = penjurusan.id_kelas
+		WHERE mengajar.id_mengajar = $id 
+		ORDER BY absen_siswa.tanggal ASC";
+
+		$data['title'] = 'Laporan Absen';
+		$data['mengajar'] = $this->db->query($queryMengajar)->result();
+		$data['absensi'] = $this->db->query($ngambilabsen)->result();
+
+		$this->load->view('users/templates/header', $data);
+		$this->load->view('users/templates/navguru');
+		$this->load->view('users/guru/report_absensi');
+		$this->load->view('users/templates/footer');
+	}
 }
--- application/views/users/guru/report_absensi.php
+++ application/views/users/guru/report_absensi.php
@@ -1,45 +1,63 @@
-<div class="course-w3ls py-5"> 
-		<div class="container py-xl-5 py-lg-3">
-			<h3 class="title text-capitalize font-weight-light text-dark text-center mb-sm-5 mb-4">Report Absensi
-			</h3>
-			<div class="row justify-content-center pt-7">
-				<div class="col-lg-10 agile-course-main">
-					<div class="w3ls-cource-first">
-						<div class="px-md-5 px-4  pb-md-5 pb-4">
-						<table class="table table-striped">
-  <thead>
-    <tr>
-      <th scope="col">Nama</th>
-      <th scope="col">NIS</th>
-      <th scope="col">P</th>
-      <th scope="col">L</th>
-	  <th scope="col">E</th>
-	  <th scope="col">A</th>
-	  <th scope="col">Sesi</th>
-	  <th scope="col">Point</th>
-	  <th scope="col">Presentase</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>Ihza OKtaviea</td>
-      <td>E41180991</td>
-	  <td>0</td>
-	  <td>0</td>
-      <td>0</td>
-      <td>0</td>
-      <td>0</td>
-      <td>0 / 0</td>
-      <td>0.0%</td>
-    </tr>
-  </tbody>
-</table>
+<div class="course-w3ls py-5">
+	<div class="container py-xl-5 py-lg-3">
+		<h3 class="title text-capitalize font-weight-light text-dark text-center mb-sm-5 mb-4">Report Absensi
+		</h3>
+		<div class="row justify-content-center pt-7">
+			<div class="col-lg-10 agile-course-main">
+				<div class="w3ls-cource-first">
+					<div class="px-md-5 px-4  pb-md-5 pb-4">
+						<br>
+						<a class="btn btn-primary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							Meta Pelajaran
+						</a>
+						<div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+							<a class="dropdown-item" href="<?= base_url('User/Guru/Report_absensi'); ?>">Semua Kelas</a>
+							<?php foreach ($mengajar as $t) :
+							?>
+								<a class="dropdown-item" href="<?= base_url('User/Guru/Report_absensi/mengajar/' . $t->id_mengajar); ?>"><?= $t->mata_pelajaran, " Di Kelas ", $t->kelas, " ", $t->nama_jurusan ?></a>
+							<?php endforeach; ?>
 						</div>
+						<br>
+						<br>
+						<br>
+						<table class="table table-striped">
+							<thead>
+								<tr>
+									<th scope="col">Nama</th>
+									<th scope="col">NIS</th>
+									<th scope="col">H</th>
+									<th scope="col">S/I</th>
+									<th scope="col">A</th>
+									<th scope="col">Point</th>
+									<th scope="col">Presentase</th>
+								</tr>
+							</thead>
+							<?php foreach ($absensi as $a) : ?>
+								<tbody>
+									<tr>
+										<td><?= $a->NAMA; ?></td>
+										<td><?= $a->nis; ?></td>
+										<td><?php if ($a->status == 2) {
+											echo '';
+										}
+										else {
+											echo '';
+										} ?></td>
+										<td>0</td>
+										<td>0</td>
+										<td>0</td>
+										<td>0 / 0</td>
+										<td>0.0%</td>
+									</tr>
+								</tbody>
+							<?php endforeach; ?>
+						</table>
 					</div>
 				</div>
-				<div class="col-lg-5 agile-course-main-2 mt-4">
-					<img src="images/am1.jpg" alt="" class="img-fluid">
-				</div>
-            </div>
+			</div>
+			<div class="col-lg-5 agile-course-main-2 mt-4">
+				<img src="images/am1.jpg" alt="" class="img-fluid">
+			</div>
 		</div>
 	</div>
+</div>

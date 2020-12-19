156432e9b66560dda31b485dd99b5c4c97474913
nafislestamanta <53818473+nafislestamanta@users.noreply.github.com>
2020-12-19 14:54:38 +0700

    mesti ketinggal wkwk

--- application/views/users/guru/dashboard.php
+++ application/views/users/guru/dashboard.php
@@ -13,7 +13,7 @@
         foreach ($absen as $a) : ?>
           <div id="ulang<?= $s; ?>">
             <img src="<?php echo base_url(); ?>assets/users/images/doc1.png" style=”float:left; margin:0 8px 4px 0;” /> <?= $a->mata_pelajaran ?> <?= $a->kelas ?> <?= $a->nama_jurusan ?>
-            <a href="#" class="btn btn-outline-danger" style="float:right;">Cek Hasil Absensi</a>
+            <a href="<?= base_url('User/Guru/Report_absensi/mengajar/' . $a->id_jurusan); ?>" class="btn btn-outline-danger" style="float:right;">Cek Hasil Absensi</a>
             <p class="card-text-left"><?= $a->tanggal ?></p>
             <hr>
           </div>

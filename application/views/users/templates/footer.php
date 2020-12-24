e8b36dde3196d7b65340bbd4633676021937ad5a
Nizarudn <muhnizarudn@gmail.com>
2020-12-24 14:20:41 +0700

    footer amang

--- application/views/users/templates/footer.php
+++ application/views/users/templates/footer.php
@@ -1,20 +1,20 @@
-	<!-- footer -->
-	<footer>
-		<div style="background-color:#1D244F" class="copyright-agiles py-3">
-			<div class="container">
-				<div class="row">
-					<p class="col-lg-8 copy-right-grids text-white text-lg-left text-center mt-lg-1">Copyright © eLearning SMAN 1 Bondowoso 2020. All rights reserved.
-					</p>
-					<!-- social icons -->
-					<div class="social-icons text-lg-right text-center col-lg-4 mt-lg-0 mt-3">
+<style>
+.footer {
+   position: fixed;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   background-color: #1D244F;
+   color: white;
+   text-align: left;
+}
+</style>
 
-					</div>
-					<!-- //social icons -->
-				</div>
-			</div>
-		</div>
-	</footer>
-	<!-- //footer -->
+<div class="footer">
+<div class="container">
+  <p class="col-lg-8 copy-right-grids text-white text-lg-left text-center mt-lg-1">Copyright © eLearning SMAN 1 Bondowoso 2020. All rights reserved.</p>
+</div>
+</div>
 
 
 	<!-- Js files -->

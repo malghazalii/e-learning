98a3f634a0576482f1713b657ffeb1cd3a19c7ed
Nizarudn <muhnizarudn@gmail.com>
2020-12-27 05:59:24 +0700

    yby

--- application/views/users/guru/wali_kelas/penilaianmapel1.php
+++ application/views/users/guru/wali_kelas/penilaianmapel1.php
@@ -13,8 +13,12 @@
     <br>
 
 
-
-    <div class="container">
+    <div class="container  py-xl-5 py-lg-3">
+  <div class="row justify-content-center pt-7">
+      <div class="col-lg-12 agile-course-main">
+        <div class="w3ls-cource-first">
+          <div class="px-md-5 px-4  pb-md-5 pb-4">
+              <br>
         <a class="btn btn-primary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             Kuis
         </a>
@@ -31,7 +35,7 @@
                   <option value="<?= $m->tanggal ?>"> <?= $m->tanggal ?></option>
                 <?php endforeach; ?>
               </select> -->
-            <button type="submit" class="btn btn-primary">Cari</button>
+            <button type="submit" class="btn btn-success">Cari</button>
         </form>
         <hr>
         <div class="panel panel-default">
@@ -42,19 +46,19 @@
                     <div class="control-group after-add-more">
                         <div class="form-group">
                             <div class="row">
-                                <a for="birthDate" class="col-sm-2 control-label">Mata Pelajaran :</a>
+                                <a for="birthDate" class="col-sm-2 control-label"><strong>Mata Pelajaran</strong></a>
                                 <label for="birthDate" class="col-sm-2 control-label"><?= $mapel->mata_pelajaran ?></label>
                             </div>
                         </div>
                         <div class="form-group">
                             <div class="row">
-                                <a for="birthDate" class="col-sm-2 control-label">Kelas Anda</a>
+                                <a for="birthDate" class="col-sm-2 control-label"><strong>Kelas Anda</strong></a>
                                 <label for="birthDate" class="col-sm-2 control-label"><?= $walikelas->kelas ?> <?= $walikelas->nama_jurusan ?></label>
                             </div>
                         </div>
                         <div class="form-group">
                             <div class="row">
-                                <a for="birthDate" class="col-sm-2 control-label">Jumlah Siswa</a>
+                                <a for="birthDate" class="col-sm-2 control-label"><stong>Jumlah Siswa</strong></a>
                                 <label for="birthDate" class="col-sm-2 control-label"><?= $sum->jumlah ?></label>
                             </div>
                         </div>
@@ -91,6 +95,10 @@
             </div>
             </form>
         </div>
+    </div>
+    </div>
+    </div>
+    </div>
     </div>
     </div>
 

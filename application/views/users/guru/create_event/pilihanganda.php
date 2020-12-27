7bb285b9f13d323cf172e2b32a2fe764f17b697a
Nizarudn <muhnizarudn@gmail.com>
2020-12-27 13:39:43 +0700

    bismillah aman

--- application/views/users/guru/create_event/pilihanganda.php
+++ application/views/users/guru/create_event/pilihanganda.php
@@ -1,36 +1,41 @@
 <div class="container  py-xl-5 py-lg-3">
-    <div class="panel panel-default">
+<div class="row justify-content-center pt-7">
+      <div class="col-lg-12 agile-course-main">
+        <div class="w3ls-cource-first">
+          <div class="px-md-5 px-4  pb-md-5 pb-4">
+             <div class="panel panel-default">
         <div class="panel-body">
             <!-- membuat form  -->
             <!-- gunakan tanda [] untuk menampung array  -->
             <div class="control-group after-add-more">
+                <br>
                 <div class="form-group">
                     <div class="row">
-                        <a for="birthDate" class="col-sm-2 control-label">Kelas Anda</a>
+                        <a for="birthDate" class="col-sm-2 control-label"><strong>Kelas Anda</strong></a>
                         <label for="birthDate" class="col-sm-2 control-label"><?= $ujian->kelas, ' ', $ujian->nama_jurusan; ?></label>
                     </div>
                 </div>
                 <div class="form-group">
                     <div class="row">
-                        <a for="birthDate" class="col-sm-2 control-label">Nama Siswa</a>
+                        <a for="birthDate" class="col-sm-2 control-label"><strong>Nama Siswa</strong></a>
                         <label for="birthDate" class="col-sm-2 control-label"><?= $ujian->NAMA; ?></label>
                     </div>
                 </div>
                 <div class="form-group">
                     <div class="row">
-                        <a for="birthDate" class="col-sm-2 control-label">Tipe Ujian</a>
+                        <a for="birthDate" class="col-sm-2 control-label"><strong>Tipe Ujian</strong></a>
                         <label for="birthDate" class="col-sm-2 control-label"><?= $ujian->jenis; ?></label>
                     </div>
                 </div>
                 <div class="form-group">
                     <div class="row">
-                        <a for="birthDate" class="col-sm-2 control-label">Nama Ujian</a>
+                        <a for="birthDate" class="col-sm-2 control-label"><strong>Nama Ujian</strong></a>
                         <label for="birthDate" class="col-sm-2 control-label"><?= $ujian->nama_ujian; ?></label>
                     </div>
                 </div>
                 <div class="form-group">
                     <div class="row">
-                        <a for="birthDate" class="col-sm-2 control-label">Jumlah Soal</a>
+                        <a for="birthDate" class="col-sm-2 control-label"><strong>Jumlah Soal</strong></a>
                         <label for="birthDate" class="col-sm-2 control-label"><?= $ujian->jml_soal; ?></label>
                     </div>
                 </div>
@@ -91,24 +96,32 @@
                                 <?php endif; ?>
                             </div>
                         </div>
+                        <div class="form-group">
+                            <div class="row">
                         <div class="col-sm-3 col-sm-offset-3">
                             <label for="nilai" class="col-sm-3 control-label">Nilai</label>
-                            <input type="number" style="width: 50px; margin-bottom: 20px;" name="nilai" value="<?= $n->nilai; ?>">
+                            <input type="number" style="width: 25%; margin-bottom: 5px;" name="nilai" value="<?= $n->nilai; ?>">
                             <input name="id_ujian" hidden type="text" style="width: 50px; margin-bottom: 20px;" value="<?= $n->id_ujian; ?>">
                             <input name="id_tr_kuis" hidden type="text" style="width: 50px; margin-bottom: 20px;" value="<?= $n->id_tr_kuis; ?>">
-                            <button type="submit" class="btn btn-primary btn-block">Lanjut</button>
+                            <button type="submit" class="btn btn-primary " >Lanjut</button>
+                        </div>
+                        </div>
                         </div>
                     </form> <!-- /form -->
                 </div>
             <?php endforeach; ?>
+            <hr>
             </br>
             <form action="<?= base_url('User/Guru/TampilKuis/updates'); ?>" method="POST">
-                <label for="TotalNilai"> Total Nilai</label>
+                <label for="TotalNilai" class="col-sm-3 control-label"> <strong>Total Nilai</strong></label>
                 <input readonly type="text" id="TotalNilai" name="nilai" value="<?= $sum->total; ?>">
                 <input hidden type="text" id="TotalNilai" name="id_ujian" value="<?= $uj->id_ujian; ?>">
-                <button type="submit" class="btn btn-primary">Selesai</button>
+                <button type="submit" class="btn btn-success">Selesai</button>
             </form>
         </div>
     </div>
+</div>
+</div>
+</div>
 </div>
 </div>

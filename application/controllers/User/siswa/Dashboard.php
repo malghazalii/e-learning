3c46e71227e8d69f38710161faf5dae61ae7020e
nafislestamanta <53818473+nafislestamanta@users.noreply.github.com>
2020-12-28 21:45:39 +0700

    done

--- application/controllers/User/siswa/Dashboard.php
+++ application/controllers/User/siswa/Dashboard.php
@@ -6,7 +6,7 @@ class Dashboard extends CI_Controller
   function __construct()
   {
     parent::__construct();
-    //cek_login_siswa();
+    cek_login_siswa();
   }
   public function index()
   {
@@ -30,7 +30,7 @@ class Dashboard extends CI_Controller
     $querymapel = "SELECT * FROM mengajar JOIN guru on guru.nip = mengajar.nip 
     JOIN mata_pelajaran on mata_pelajaran.id_mapel = mengajar.id_mapel 
     JOIN penjurusan on penjurusan.id_jurusan = mengajar.id_jurusan 
-    JOIN kelas on kelas.id_kelas = penjurusan.id_kelas WHERE mengajar.id_jurusan = $nis";
+    JOIN kelas on kelas.id_kelas = penjurusan.id_kelas WHERE mengajar.id_jurusan = $s->id_jurusan";
 
     $querykuis = "SELECT * FROM kuis JOIN mengajar on mengajar.id_mengajar = kuis.id_mengajar 
     JOIN guru on guru.nip = mengajar.nip JOIN mata_pelajaran on mata_pelajaran.id_mapel = mengajar.id_mapel 
@@ -68,6 +68,72 @@ class Dashboard extends CI_Controller
     $this->load->view('users/templates/header', $data);
     $this->load->view('users/templates/navsiswa');
     $this->load->view('users/siswa/dashboard');
+    $this->load->view('users/templates/footer');
+    $this->load->view('auto');
+  }
+
+  public function mapel($id)
+  {
+    $nis = $this->session->userdata('nis');
+
+    $siswa = "SELECT * FROM siswa JOIN penjurusan on penjurusan.id_jurusan = siswa.id_jurusan JOIN kelas on kelas.id_kelas = penjurusan.id_kelas WHERE siswa.nis=$nis";
+
+    $s = $this->db->query($siswa)->row();
+
+    $querytugas = "SELECT *, tugas_siswa.nama as NAMA FROM tugas_siswa JOIN mengajar on mengajar.id_mengajar = tugas_siswa.id_mengajar 
+    JOIN guru on guru.nip = mengajar.nip JOIN mata_pelajaran on mata_pelajaran.id_mapel = mengajar.id_mapel 
+    JOIN penjurusan on penjurusan.id_jurusan = mengajar.id_jurusan JOIN kelas on kelas.id_kelas = penjurusan.id_kelas WHERE NOT EXISTS
+    (SELECT * FROM jawaban_tugas WHERE tugas_siswa.id_tugas=jawaban_tugas.id_tugas and jawaban_tugas.nis=$nis) 
+    AND penjurusan.id_jurusan = $s->id_jurusan and tugas_siswa.id_mengajar=$id";
+
+
+    $tugastelat = "SELECT *, tugas_siswa.nama as NAMA FROM jawaban_tugas JOIN tugas_siswa on tugas_siswa.id_tugas = jawaban_tugas.id_tugas JOIN mengajar on mengajar.id_mengajar = tugas_siswa.id_mengajar 
+    JOIN guru on guru.nip = mengajar.nip JOIN mata_pelajaran on mata_pelajaran.id_mapel = mengajar.id_mapel 
+    JOIN penjurusan on penjurusan.id_jurusan = mengajar.id_jurusan JOIN kelas on kelas.id_kelas = penjurusan.id_kelas WHERE jawaban_tugas.nis = $nis and jawaban_tugas.status = 3 and tugas_siswa.id_mengajar=$id";
+
+    $querymapel = "SELECT * FROM mengajar JOIN guru on guru.nip = mengajar.nip 
+    JOIN mata_pelajaran on mata_pelajaran.id_mapel = mengajar.id_mapel 
+    JOIN penjurusan on penjurusan.id_jurusan = mengajar.id_jurusan 
+    JOIN kelas on kelas.id_kelas = penjurusan.id_kelas WHERE mengajar.id_jurusan = $s->id_jurusan";
+
+    $querymapell = "SELECT * FROM mengajar JOIN guru on guru.nip = mengajar.nip 
+    JOIN mata_pelajaran on mata_pelajaran.id_mapel = mengajar.id_mapel 
+    JOIN penjurusan on penjurusan.id_jurusan = mengajar.id_jurusan 
+    JOIN kelas on kelas.id_kelas = penjurusan.id_kelas WHERE mengajar.id_mengajar=$id";
+
+    $querykuis = "SELECT * FROM kuis JOIN mengajar on mengajar.id_mengajar = kuis.id_mengajar 
+    JOIN guru on guru.nip = mengajar.nip JOIN mata_pelajaran on mata_pelajaran.id_mapel = mengajar.id_mapel 
+    JOIN penjurusan on penjurusan.id_jurusan = mengajar.id_jurusan JOIN kelas on kelas.id_kelas = penjurusan.id_kelas WHERE mengajar.id_jurusan = $s->id_jurusan";
+
+    $queryabsen = "SELECT * FROM absen_siswa 
+    JOIN mengajar ON mengajar.id_mengajar = absen_siswa.id_mengajar
+    JOIN penjurusan ON penjurusan.id_jurusan = mengajar.id_jurusan
+    JOIN kelas ON kelas.id_kelas = penjurusan.id_kelas
+    JOIN mata_pelajaran ON mata_pelajaran.id_mapel = mengajar.id_mapel
+    WHERE NOT EXISTS
+    (SELECT * FROM tr_absen_siswa WHERE absen_siswa.id_absen = tr_absen_siswa.id_absen AND tr_absen_siswa.nis = $nis)
+    AND penjurusan.id_jurusan = $s->id_jurusan and absen_siswa.id_mengajar=$id";
+
+    $querykuiss = "SELECT * FROM kuis 
+      JOIN mengajar ON mengajar.id_mengajar = kuis.id_mengajar
+      JOIN penjurusan ON penjurusan.id_jurusan = mengajar.id_jurusan
+      JOIN kelas ON kelas.id_kelas = penjurusan.id_kelas
+      JOIN mata_pelajaran ON mata_pelajaran.id_mapel = mengajar.id_mapel
+      WHERE NOT EXISTS
+      (SELECT * FROM ikut_ujian WHERE kuis.id_kuis = ikut_ujian.id_kuis AND ikut_ujian.nis = $nis)
+      AND penjurusan.id_jurusan = $s->id_jurusan and kuis.id_mengajar=$id";
+
+    $data['title'] = 'Dashboard';
+    $data['data'] = $this->db->get_where('siswa', ['nis' => $this->session->userdata('nis')])->row_array();
+    $data['absensi'] = $this->db->query($queryabsen)->result();
+    $data['mapel'] = $this->db->query($querymapel)->result();
+    $data['mapell'] = $this->db->query($querymapell)->row();
+    $data['tugastelat'] = $this->db->query($tugastelat)->result();
+    $data['tugas'] = $this->db->query($querytugas)->result();
+    $data['kuis'] = $this->db->query($querykuiss)->result();
+    $this->load->view('users/templates/header', $data);
+    $this->load->view('users/templates/navsiswa');
+    $this->load->view('users/siswa/dashboard1');
     $this->load->view('users/templates/footer');
     $this->load->view('auto');
   }

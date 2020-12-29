e234477efbab71cd85efc9f66396b86bc3467179
Ihza Oktavira <57393641+ihza149@users.noreply.github.com>
2020-12-29 20:34:24 +0700

    Beberapa Bug, Header footer

--- application/views/users/siswa/f_tugas2.php
+++ application/views/users/siswa/f_tugas2.php
@@ -1,9 +1,9 @@
 <html lang="en">
-
+<!-- 
 <head>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-</head>
+</head> -->
 
 <body>
 <div class="container  py-xl-5 py-lg-3">

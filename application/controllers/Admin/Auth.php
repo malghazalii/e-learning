fc5e1c2073b0aac45090a06972f29d3bf31512d8
nafislestamanta <53818473+nafislestamanta@users.noreply.github.com>
2021-01-02 13:20:03 +0700

    lupa password admin

--- application/controllers/Admin/Auth.php
+++ application/controllers/Admin/Auth.php
@@ -68,4 +68,126 @@ class Auth extends CI_Controller
     $this->load->view('blok', $data);
     $this->load->view('Admin/templates/footer');
   }
+
+  private function _sendEmail($token, $type)
+  {
+    $config = [
+      'protocol' => 'smtp',
+      'smtp_host' => 'ssl://smtp.googlemail.com',
+      'smtp_user' => 'Sman1bondowoso@gmail.com',
+      'smtp_pass' => 'sman01jaya',
+      'smtp_port' => 465,
+      'mailtype' => 'html',
+      'charset' => 'utf-8',
+      'newline' => "\r\n"
+    ];
+
+    $this->load->library('email', $config);
+    $this->email->initialize($config);
+
+    $this->email->from('redshop0990@gmail.com', 'Admin Sman 1 Bondowoso');
+    $this->email->to($this->input->post('email'));
+
+    if ($type == 'forgot') {
+      $this->email->subject('Reset Password');
+      $this->email->message('Klik tautan ini untuk mengatur ulang password anda : <a
+      href="' . base_url() . 'admin/auth/resetpassword?email=' . $this->input->post('email') . '&token=' . urlencode($token) . '">Reset Password</a>');
+    }
+
+    if ($this->email->send()) {
+      return true;
+    } else {
+      echo $this->email->print_debugger();
+    }
+  }
+
+  public function forgotPassword()
+  {
+    $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email', [
+      'required' => 'Field tidak boleh kosong',
+      'valid_email' => 'Harus berisi alamat email yang valid'
+    ]);
+
+    if ($this->form_validation->run() == false) {
+      $data['title'] = 'Lupa Password';
+      $this->load->view('admin/forgot-password');
+    } else {
+      $email = $this->input->post('email');
+      $user = $this->db->get_where('admin', ['email' => $email])->row_array();
+
+      if ($user) {
+        $token = base64_encode(random_bytes(32));
+        $user_token = [
+          'email' => $email,
+          'token' => $token,
+          'date_created' => time()
+        ];
+
+        $this->db->insert('user_token', $user_token);
+        $this->_sendEmail($token, 'forgot');
+
+        $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Please cek email anda untuk reset password!</div>');
+        redirect('Admin/auth/forgotpassword');
+      } else {
+        $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Email tidak tersedia!</div>');
+        redirect('Admin/auth/forgotpassword');
+      }
+    }
+  }
+
+  public function resetPassword()
+  {
+    $email = $this->input->get('email');
+    $token = $this->input->get('token');
+
+    $user = $this->db->get_where('admin', ['email' => $email])->row_array();
+
+    if ($user) {
+      $user_token = $this->db->get_where('user_token', ['token' => $token])->row_array();
+
+      if ($user_token) {
+        $this->session->set_userdata('reset_email', $email);
+        $this->changePassword();
+      } else {
+        $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert"> Reset password gagal! Token salah</div>');
+        redirect('Admin/auth');
+      }
+    } else {
+      $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert"> Reset password gagal! Email salah</div>');
+      redirect('Admin/auth');
+    }
+  }
+
+  public function changePassword()
+  {
+    if (!$this->session->userdata('reset_email')) {
+      redirect('admin/auth');
+    }
+
+    $this->form_validation->set_rules('password1', 'Password', 'trim|required|min_length[6]|matches[password2]', [
+      'required' => 'Field tidak boleh kosong',
+      'min_length' => 'Minimal password 6 karakter',
+      'matches' => 'password tidak cocok dengan ulangi password'
+    ]);
+    $this->form_validation->set_rules('password2', 'Repeat Password', 'trim|required|min_length[6]|matches[password1]', [
+      'required' => 'Field tidak boleh kosong',
+      'min_length' => 'Minimal password 6 karakter',
+      'matches' => 'Ulangi password tidak cocok dengan password baru'
+    ]);
+    if ($this->form_validation->run() == false) {
+      $data['title'] = 'Ubah Password';
+      $this->load->view('admin/change-password');
+    } else {
+      $password = $this->input->post('password1');
+      $email = $this->session->userdata('reset_email');
+
+      $this->db->set('password', md5($password));
+      $this->db->where('email', $email);
+      $this->db->update('admin');
+
+      $this->session->unset_userdata('reset_email');
+      $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Password telah diubah! Mohon login</div>');
+      redirect('Admin/auth');
+    }
+  }
 }
--- application/views/admin/login.php
+++ application/views/admin/login.php
@@ -52,7 +52,7 @@
                   </form>
                   <hr>
                   <div class="text-center">
-                    <a class="small" href="forgot-password.html">Lupa Password?</a>
+                    <a class="small" href="<?= base_url('admin/auth/forgotpassword'); ?>">Lupa Password?</a>
                   </div>
                 </div>
               </div>

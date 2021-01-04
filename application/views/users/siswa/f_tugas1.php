1d9b86785c3f722feb234f55f651e20e77d890d3
nafislestamanta <53818473+nafislestamanta@users.noreply.github.com>
2021-01-04 12:07:40 +0700

    ..

--- application/views/users/siswa/f_tugas1.php
+++ application/views/users/siswa/f_tugas1.php
@@ -143,40 +143,36 @@
               </tr>
               <tr>
                 <td>Waktu yang tersisa</td>
-                <?php if ($satu) : ?>
-                  <td style="background-color: lightseagreen;">Tepat Waktu</td>
-                <?php else : ?>
-                  <td id="demo" style="background-color: lightseagreen;"></td>
-                  <script>
-                    // Mengatur waktu akhir perhitungan mundur
-                    var countDownDate = new Date("<?= $tugas->TANGGAL; ?>").getTime();
+                <td id="demo" style="background-color: lightseagreen;"></td>
+                <script>
+                  // Mengatur waktu akhir perhitungan mundur
+                  var countDownDate = new Date("<?= $tugas->TANGGAL; ?>").getTime();
 
-                    // Memperbarui hitungan mundur setiap 1 detik
-                    var x = setInterval(function() {
+                  // Memperbarui hitungan mundur setiap 1 detik
+                  var x = setInterval(function() {
 
-                      // Untuk mendapatkan tanggal dan waktu hari ini
-                      var now = new Date().getTime();
+                    // Untuk mendapatkan tanggal dan waktu hari ini
+                    var now = new Date().getTime();
 
-                      // Temukan jarak antara sekarang dan tanggal hitung mundur
-                      var distance = countDownDate - now;
+                    // Temukan jarak antara sekarang dan tanggal hitung mundur
+                    var distance = countDownDate - now;
 
-                      // Perhitungan waktu untuk hari, jam, menit dan detik
-                      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-                      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-                      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-                      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+                    // Perhitungan waktu untuk hari, jam, menit dan detik
+                    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+                    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+                    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-                      // Keluarkan hasil dalam elemen dengan id = "demo"
-                      document.getElementById("demo").innerHTML = days + "d " + hours + "h " +
-                        minutes + "m " + seconds + "s ";
+                    // Keluarkan hasil dalam elemen dengan id = "demo"
+                    document.getElementById("demo").innerHTML = days + "d " + hours + "h " +
+                      minutes + "m " + seconds + "s ";
 
-                      // Jika hitungan mundur selesai, tulis beberapa teks 
-                      if (distance < 0) {
-                        document.getElementById("demo").innerHTML = "Tepat Waktu";
-                      }
-                    }, 1000);
-                  </script>
-                <?php endif; ?>
+                    // Jika hitungan mundur selesai, tulis beberapa teks 
+                    if (distance < 0) {
+                      document.getElementById("demo").innerHTML = "Tepat Waktu";
+                    }
+                  }, 1000);
+                </script>
               </tr>
               <tr>
                 <td>terakhir diubah</td>
